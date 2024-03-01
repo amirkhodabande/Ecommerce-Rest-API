b9b2aa71c393eaa1d77adc2c737f28d59bde065e
Amir Khodabande <amirdp8102@gmail.com>
2024-03-01 19:09:54 +0330

    add update to user routes and fix some issues

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,34 @@
 const { StatusCodes } = require("http-status-codes");
 const User = require("../models/User");
 const Error = require("../errors");
+const utils = require('../utils');
 
-const updatePassword = async (req, res) => {
-    const { email, oldPassword, newPassword } = req.body;
+const update = async (req, res) => {
+    const { email, name } = req.body;
 
-    if (!email || !oldPassword || !newPassword ) {
-        throw new Error.BadRequest('email and password are required!');
+    if (!email || !name) {
+        throw new Error.BadRequest('email and name are required!');
     }
 
-    const user = await User.findOne({ email });
+    const user = await User.findByIdAndUpdate(
+        req.user.userId,
+        { email, name },
+        {new: true, runValidators: true}
+    );
+
+    const token = utils.createToken(user);
+
+    res.status(StatusCodes.CREATED).json({ token });
+}
+
+const updatePassword = async (req, res) => {
+    const {oldPassword, newPassword } = req.body;
+
+    if (!oldPassword || !newPassword) {
+        throw new Error.BadRequest('password is required!');
+    }
+
+    const user = await User.findById(req.user.userId);
 
     if (!user) {
         throw new Error.BadRequest('Wrong credentials!');
@@ -23,9 +42,10 @@ const updatePassword = async (req, res) => {
 
     await user.save();
 
-    res.status(StatusCodes.OK).json({ message: 'Password updated successfully!'});
+    res.status(StatusCodes.OK).json({ message: 'Password updated successfully!' });
 }
 
 module.exports = {
+    update,
     updatePassword
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const {
-    updatePassword 
+    update, updatePassword 
 } = require('../controllers/userController');
 
+router.put('/', update);
 router.put('/update-password', updatePassword);
 
 module.exports = router;
